180a70d4b37698c94147000b02f30b2f531b9684
Robert-Hirgau <58361920+Robert-Hirgau@users.noreply.github.com>
2019-12-10 17:27:48 +0200

    Add files via upload

---
+++ homework/test/src/Main.java
@@ -0,0 +1,14 @@
+import org.joda.time.DateTime;
+
+public class Main {
+    public Main() {
+}
+
+public static void main(String[] args){
+DateTime date = new DateTime();
+System.out.println(date.toString());
+}
+}
+
+
+
